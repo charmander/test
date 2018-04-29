d8710203268e0fc69a3106676416b12a4a17f54f
Charmander <~@charmander.me>
2018-04-28 22:02:54 -0700

    Add generic, nestable test grouping

--- describe.js
+++ describe.js
@@ -2,22 +2,5 @@
 
 const getTest = require('./');
 
-module.exports = testModule => {
-	const test = getTest(testModule);
-
-	return (what, description) => {
-		if (typeof what !== 'string') {
-			throw new TypeError('Test name must be a string');
-		}
-
-		const it = (behaviour, run) => {
-			if (typeof behaviour !== 'string') {
-				throw new TypeError('Test name must be a string');
-			}
-
-			test(what + ' › ' + behaviour, run);
-		};
-
-		description(it);
-	};
-};
+module.exports = testModule =>
+	getTest(testModule).group;
--- index.js
+++ index.js
@@ -6,6 +6,43 @@ const defaultOutput = require('./internal/output');
 
 let runScheduled = false;
 
+const runMainTests = () => {
+	const tests = Object.freeze(require.main.exports);
+	run(defaultOutput, tests);
+};
+
+const addTestWithPrefix = (testModule, prefix) => {
+	const addTest = (name, run) => {
+		if (typeof name !== 'string') {
+			throw new TypeError('Test name must be a string');
+		}
+
+		if (!Object.isExtensible(testModule.exports)) {
+			throw new Error('Can’t add tests during test run');
+		}
+
+		const prefixedName = [...prefix, name].join(' › ');
+
+		testModule.exports.push(new Test(prefixedName, run));
+	};
+
+	const addGroup = (name, contents) => {
+		if (typeof name !== 'string') {
+			throw new TypeError('Group name must be a string');
+		}
+
+		const addGroupedTest = addTestWithPrefix(testModule, [...prefix, name]);
+		contents(addGroupedTest);
+	};
+
+	Object.defineProperty(addTest, 'group', {
+		configurable: true,
+		value: addGroup,
+	});
+
+	return addTest;
+};
+
 module.exports = testModule => {
 	if (!Array.isArray(testModule.exports)) {
 		testModule.exports = [];
@@ -13,18 +50,8 @@ module.exports = testModule => {
 
 	if (testModule === require.main && !runScheduled) {
 		runScheduled = true;
-
-		process.nextTick(() => {
-			const tests = Object.freeze(testModule.exports);
-			run(defaultOutput, tests);
-		});
+		process.nextTick(runMainTests);
 	}
 
-	return (name, run) => {
-		if (!Object.isExtensible(testModule.exports)) {
-			throw new Error('Can’t add tests during test run');
-		}
-
-		testModule.exports.push(new Test(name, run));
-	};
+	return addTestWithPrefix(testModule, []);
 };
